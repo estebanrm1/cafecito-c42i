75c7016579f7356e9914374ecf81c8a22f5e2b82
estebanrm1 <solsel90@gmail.com>
2023-07-06 16:46:42 -0300

    Se guardaron los cambios de inicio y card de productos

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,6 +1,28 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Spinner } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
+import { obtenerListaProductos } from "../helpers/queries";
+import { useState, useEffect } from "react";
+
 const Inicio = () => {
+
+  const [productos, setProductos] = useState([]);
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
+
+  useEffect(() => {
+      obtenerListaProductos().then((respuesta) => {
+          if (respuesta) {
+              setProductos(respuesta);
+              setMostrarSpinner(false);
+          } else {
+              Swal.fire({
+                  title: "Hubo un error!",
+                  text: "Intente Realizar esta operacion en un momento por favor",
+                  icon: 'error'
+              })
+          }
+      })
+  }, [])
+
   return (
     <section className="mainSection">
       <img
@@ -11,12 +33,17 @@ const Inicio = () => {
       <Container>
         <h1 className="display-4">Nuestros Productos</h1>
         <hr />
-        <Row>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-        </Row>
+        <Row xs={1} md={2} lg={3} xl={4} className="justify-content-center align-items-stretch">
+                    {mostrarSpinner ? (
+                        <div className="my-3 text-center">
+                            <Spinner animation="grow" variant="danger"></Spinner>
+                        </div>
+                    ) : (
+                        productos.map((producto) => (
+                            <CardProducto key={producto.id} producto={producto}></CardProducto>
+                        ))
+                    )}
+                </Row>
       </Container>
     </section>
   );
